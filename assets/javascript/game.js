6f06206d15c6dffbe75d1f688acd2b1c610bccd5
landon <landon.seebart@gmail.com>
2018-02-28 19:59:20 -0600

    created computerPlayer object and added choices array

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+
+    // const playerInput = prompt();
+
+const computerPlayer = {
+    computerChoices: ['a','b','c','d','e','f','g','h','i','j','k','l',
+    'm','n','o','p','q','r','s','t','u','v','w','x','y','z']
+};
+
+//     const random_letter = function() {
+//         var chars = "0123456789abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ";
+//         return chars.substr( Math.floor(Math.random() * 62), 1);
+//     }
+
+
+// const computerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l',
+// 'm','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+console.log(computerPlayer.computerChoices);
