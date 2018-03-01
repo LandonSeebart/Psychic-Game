50c3a5904e4f12debdf1e09fcc6bf33c1e77ec26
landon <landon.seebart@gmail.com>
2018-02-28 21:30:44 -0600

    finished player and computer objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,19 +4,19 @@ const computerPlayer = {
     computerChoices: ['a','b','c','d','e','f','g','h','i','j','k','l',
     'm','n','o','p','q','r','s','t','u','v','w','x','y','z'],
     computerChoiceSelecter: function(arg1) {
-        return (arg1[Math.floor(Math.random() * arg1.length)]);
+       return (arg1[Math.floor(Math.random() * arg1.length)]);
     }
 };
 
-let computerSelection = computerPlayer.computerChoiceSelecter(computerPlayer.computerChoices)
+const a = computerPlayer.computerChoiceSelecter(computerPlayer.computerChoices);
 
-const humanPlayer = {
-    guess: "a",
-    playerGuesses: [ ],
-    recordPlayerInput: function(arg1) {
-       humanPlayer.playerGuesses.push(arg1);
-    }
-};
+// const humanPlayer = {
+//     guess: "a",
+//     playerGuesses: [ ],
+//     recordPlayerInput: function(arg1) {
+//        humanPlayer.playerGuesses.push(arg1);
+//     }
+// };
 
-console.log(humanPlayer.playerGuesses);
+console.log(a);
 
