2c20747fcfb5ce125bbc815b266c26e59e766828
landon <landon.seebart@gmail.com>
2018-03-01 18:33:40 -0600

    added wins, losses, and score count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,96 +1,77 @@
+'use strict';
 
+// Declare variables
 
-// const computerPlayer = {
-//     const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
-//     'm','n','o','p','q','r','s','t','u','v','w','x','y','z']
-//     computerChoiceSelecter: function(arg1) {
-//        return (arg1[Math.floor(Math.random() * arg1.length)]);
-//     },
-// };
-
-// const humanPlayer = {
-//     guess: "a",
-//     playerGuesses: [ ],
-//     recordPlayerInput: function(arg1) {
-//        humanPlayer.playerGuesses.push(arg1);
-//     }
-// };
-
-// const scoreKeeper = {
-//     evaluateChoices: function(arg1, arg2) {
-//     },
-//     updateScore: function {
-
-//     }
-
-// }
-
-// const a = computerPlayer.computerChoiceSelecter(computerPlayer.computerChoices);
-
-
-const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
-'m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 const randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+let guessCount = 1;
 
-// var guesses = document.querySelector('.guesses');
-// var lastResult = document.querySelector('.lastResult');
-// var guessSubmit = document.querySelector('.guessSubmit');
-// var guessField = document.querySelector('.guessField');
-// var guessCount = 1;
-// var resetButton;
+//Links to html ids
+const guesses = document.querySelector('.guesses');
+const guessesLeft = document.querySelector('.guessesLeft');
+const scoreWins = document.querySelector('.wins');
+const scoreLosses = document.querySelector('losses');
+const lastResult = document.querySelector('.lastResult');
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
 
-
+//checks to see if user guess matches random leter
 
 function checkGuess() {
-var userGuess = a;
-//   if (guessCount === 1) {
-//     guesses.textContent = 'Previous guesses: ';
-//   }
-//   guesses.textContent += userGuess + ' ';
+  let userGuess = (guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = 'Previous guesses: ';
+    guessesLeft.textContent = 'Guesses left: ';
+  }
+
+  guesses.textContent += userGuess + ' ';
+  guessesLeft.textContent += guessCount + ' ';
  
-//   if (userGuess === randomLetter) {
-//     lastResult.textContent = 'Yup!';
-//     setGameOver();
-//   } else if (guessCount === 10) {
-//     lastResult.textContent = '!!!GAME OVER!!!';
-//     setGameOver();
-//  } else {
-//     lastResult.textContent = 'Wrong!';
-//   }
+  if (userGuess === randomLetter) {
+    lastResult.textContent = 'You Win!';
+    setGameOver();
+
+  } else if (guessCount === 10) {
+    lastResult.textContent = '!!!GAME OVER!!!';
+
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Wrong!';
+  }
  
-//   guessCount++;
-//   guessField.value = '';
-//   guessField.focus();
-// }
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+}
 
-// guessSubmit.addEventListener('click', checkGuess);
+//Evaluate match when user clicks
+guessSubmit.addEventListener('click', checkGuess);
 
-// function setGameOver() {
-//   guessField.disabled = true;
-//   guessSubmit.disabled = true;
-//   resetButton = document.createElement('button');
-//   resetButton.textContent = 'Start new game';
-//   document.body.appendChild(resetButton);
-//   resetButton.addEventListener('click', resetGame);
-// }
+//Stop game so that user can't enter any more inputs
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Start new game';
+  document.body.appendChild(resetButton);
+  resetButton.addEventListener('click', resetGame);
+}
 
-// function resetGame() {
-//   guessCount = 1;
+//Reset the game so a player can start again
+function resetGame() {
+  guessCount = 1;
 
-//   var resetParas = document.querySelectorAll('.resultParas p');
-//   for (var i = 0 ; i < resetParas.length ; i++) {
-//     resetParas[i].textContent = '';
-//   }
+  var resetParas = document.querySelectorAll('.resultParas p');
+  for (var i = 0 ; i < resetParas.length ; i++) {
+    resetParas[i].textContent = '';
+  }
 
-//   resetButton.parentNode.removeChild(resetButton);
+  resetButton.parentNode.removeChild(resetButton);
 
-//   guessField.disabled = false;
-//   guessSubmit.disabled = false;
-//   guessField.value = '';
-//   guessField.focus();
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
 
-//   lastResult.style.backgroundColor = 'white';
-
-//   randomLetter = alphabet(Math.floor(Math.random() * alphabet.length));
-// }
+  randomLetter = alphabet(Math.floor(Math.random() * alphabet.length));
+}
